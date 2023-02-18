b1e5d522e866d5bdc1b6e3ea0a718a09b8cc33ac
PPekKunGz <kittipat25er@gmail.com>
2023-02-18 23:54:52 +0700

    test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>ประวัตินักเรียน - FriendsCraftSMP.SS3</title>
+        <link
+          rel="icon"
+          href=""
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
