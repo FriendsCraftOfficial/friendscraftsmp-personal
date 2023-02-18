3085da73b06c012145afcbb729ae18333e3dcabd
PPekKunGz <kittipat25er@gmail.com>
2023-02-19 00:05:30 +0700

    seo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,9 +6,31 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>ประวัตินักเรียน - FriendsCraftSMP.SS3</title>
-        <link
-          rel="icon"
-          href=""
+        <link rel="icon" href="/logo.png" />
+
+        {/* <!-- SEO --> */}
+        <meta name="author" content="PPekKunGz Dev" />
+        <meta
+          name="keywords"
+          content="PPekKunGzDev,PPekKunGz_Dev,เพ็กคุง,รับทำเว็บไซต์,รับทำเว็บไซต์ราคาถูก,รับทำเว็บไซต์อื่นๆ,เว็บไซต์PPekKunGz,เพ็กคุงทำเว็บ"
+        />
+
+        <meta property="og:local" content="th_TH" />
+        <meta property="og:url" content="https://personal.ppekkungz.online" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="FriendsCraftSMP SS3 - High School" />
+        <meta
+          property="og:description"
+          content="📗 เว็ปไซต์แสดงประวัตินักเรียนในโรงเรียน "
+        />
+        <meta
+          property="og:image"
+          content="https://personal.ppekkungz.online/logo.png"
+        />
+        <meta property="og:url" content="https://personal.ppekkungz.online/" />
+        <meta
+          property="og:site_name"
+          content="FriendsCraft Official, @PPekKunGzDev"
         />
       </Head>
       <Component {...pageProps} />
